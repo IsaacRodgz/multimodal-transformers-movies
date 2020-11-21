5bc7a4b2a08b9234074a2caf6b80c37e302afd5e
IsaacRodgz <chack.rb@gmail.com>
2020-11-21 17:19:22 -0600

    MODIFY Adapter module

--- mmbt/models/mmbtadapter.py
+++ mmbt/models/mmbtadapter.py
@@ -283,6 +283,42 @@ class PreTrainedBertModel(nn.Module):
         return model
 
 
+class Activation_Function_Class(nn.Module):
+    """
+    Implementation of various activation function.
+    """
+
+    def __init__(self, hidden_act):
+
+        if hidden_act.lower() == "relu":
+            self.f = nn.functional.relu
+        elif hidden_act.lower() == "tanh":
+            self.f = torch.tanh
+        elif hidden_act.lower() == "swish":
+
+            def swish(x):
+                return x * torch.nn.functional.sigmoid(x)
+
+            self.f = swish
+        elif hidden_act.lower() == "gelu":
+
+            def gelu_new(x):
+                """
+                Implementation of the gelu activation function currently in Google Bert repo (identical to OpenAI GPT).
+                Also see https://arxiv.org/abs/1606.08415
+                """
+                return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+
+            self.f = gelu_new
+        elif hidden_act.lower() == "leakyrelu":
+            self.f = nn.functional.leaky_relu
+
+        super().__init__()
+
+    def forward(self, x):
+        return self.f(x)
+
+
 class BertAdapter(nn.Module):
     """
     This module implements the "Parameter-Efficient Transfer Learning for NLP" paper:
@@ -291,8 +327,15 @@ class BertAdapter(nn.Module):
 
     def __init__(self, hidden_size, adapter_size):
         super(BertAdapter, self).__init__()
+        seq_list = []
+        
         self.layer_norm_before = nn.LayerNorm(hidden_size)
-        self.adapter_down = nn.Linear(hidden_size, adapter_size)
+        seq_list.append(self.layer_norm_before)
+        seq_list.append(nn.Linear(hidden_size, adapter_size))
+        self.non_linearity = Activation_Function_Class("gelu")
+        seq_list.append(self.non_linearity)
+        
+        self.adapter_down = nn.Sequential(*seq_list)
         self.adapter_up = nn.Linear(adapter_size, hidden_size)
         
         self.adapter_down.apply(self.init_bert_weights)
