0eda9ebed99ea88b75ff69cedf2424a98e6867b5
IsaacRodgz <chack.rb@gmail.com>
2021-01-21 19:33:04 -0600

    ADD mmtrvap model with Concat fusion

--- mmbt/models/__init__.py
+++ mmbt/models/__init__.py
@@ -17,7 +17,7 @@ from mmbt.models.mmbtadapter import MultimodalBertAdapterClf
 from mmbt.models.mmbtadapterm import MultimodalBertAdapterMClf, MultimodalBertAdapterMTropesClf
 from mmbt.models.mmbt3 import MultimodalBertThreeClf
 from mmbt.models.gmu import GMUClf
-from mmbt.models.mmtr import MMTransformerClf, MMTransformerGMUClf, MMTransformerUniClf, TransformerClf, MMTransformerUniBi, TransformerVideoClf, MMTransformerMoviescopeClf, MMTransformerGMUMoviescopeVidTextClf, MMTransformerGMUMoviescopeVidAudClf, MMTransformerConcatMoviescopeVidAudClf, MMTransformerGMUMoviescopeVidAudPosterClf, MMTransformerGMUMoviescopeVidAudPosterTxtClf, MMTransformerConcatMoviescopeVidAudPosterTxtClf, MMTransformerGMUMoviescopeClf, MMTransformerConcatMoviescopeClf, MMTransformerGMUVPAClf, MMTransformerConcatVPAClf, MMTransformerGMU4MoviescopeClf, MMTransformerConcat4MoviescopeClf, MMTransformerConcat5MoviescopeClf, MMTransformerGMU5MoviescopeClf
+from mmbt.models.mmtr import MMTransformerClf, MMTransformerGMUClf, MMTransformerUniClf, TransformerClf, MMTransformerUniBi, TransformerVideoClf, MMTransformerMoviescopeClf, MMTransformerGMUMoviescopeVidTextClf, MMTransformerGMUMoviescopeVidAudClf, MMTransformerConcatMoviescopeVidAudClf, MMTransformerGMUMoviescopeVidAudPosterClf, MMTransformerConcatMoviescopeVidAudPosterClf, MMTransformerGMUMoviescopeVidAudPosterTxtClf, MMTransformerConcatMoviescopeVidAudPosterTxtClf, MMTransformerGMUMoviescopeClf, MMTransformerConcatMoviescopeClf, MMTransformerGMUVPAClf, MMTransformerConcatVPAClf, MMTransformerGMU4MoviescopeClf, MMTransformerConcat4MoviescopeClf, MMTransformerConcat5MoviescopeClf, MMTransformerGMU5MoviescopeClf
 from mmbt.models.mmbtp import MultimodalBertTransfClf
 from mmbt.models.mmdbt import MultimodalDistilBertClf
 from mmbt.models.vilbert import VILBertForVLTasks
@@ -41,7 +41,7 @@ MODELS = {
     "mmtr": TransformerVideoClf,
     "mmtrv": MMTransformerMoviescopeClf, # text-video (MMTransformerGMUMoviescopeVidTextClf)
     "mmtrva": MMTransformerConcatMoviescopeVidAudClf, # video-audio (no text) (MMTransformerGMUMoviescopeVidAudClf)
-    "mmtrvap": MMTransformerGMUMoviescopeVidAudPosterClf, # video-audio-poster (no text)
+    "mmtrvap": MMTransformerConcatMoviescopeVidAudPosterClf, # video-audio-poster (no text) (MMTransformerGMUMoviescopeVidAudPosterClf)
     "mmtrvapt": MMTransformerConcatMoviescopeVidAudPosterTxtClf, # video-audio-poster-plot (MMTransformerGMUMoviescopeVidAudPosterTxtClf)
     "mmtrvpp": MMTransformerConcatMoviescopeClf, # video-plot-poster (MMTransformerGMUMoviescopeClf)
     "mmtrvpa": MMTransformerConcatVPAClf, # video-plot-audio (MMTransformerGMUVPAClf)
